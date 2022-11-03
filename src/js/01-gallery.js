6ec792bf6f505e8e078248c0bd4df5170ba21c7e
Mykola Saliuk <haker0502@ukr.net>
2022-11-03 22:14:04 +0200

    додає код 1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryRefsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryRefsMarkup(galleryItems)
+);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
