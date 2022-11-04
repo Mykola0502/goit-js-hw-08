f314481c786b42df0aea91e25c5784b2cc0e86fe
Mykola Saliuk <haker0502@ukr.net>
2022-11-04 03:30:41 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/01-gallery.css';
 
 console.log(galleryItems);
 
