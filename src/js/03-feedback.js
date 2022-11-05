b228aa78231d7ab66ff2ed802887d96077499f7c
Mykola Saliuk <haker0502@ukr.net>
2022-11-05 21:42:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,43 +10,128 @@
 //    Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
 import throttle from 'lodash.throttle';
+// const throttle = require('lodash.throttle');
 import '../css/03-feedback.css';
 import '../css/common.css';
 
+/**
+ *      По прикладу Олександра Репети
+ */
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   input: document.querySelector('.feedback-form  input'),
+//   textarea: document.querySelector('.feedback-form  textarea'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+// let formData = {};
+
+// populateForm();
+
+// function onFormInput(evt) {
+//   formData[evt.target.name] = evt.target.value;
+
+//   console.log(formData);
+
+//   const stringFormData = JSON.stringify(formData);
+
+//   localStorage.setItem(STORAGE_KEY, stringFormData);
+// }
+
+// function populateForm() {
+//   const savedstringFormData = localStorage.getItem(STORAGE_KEY);
+
+//   //   console.log(formData);
+
+//   if (savedstringFormData) {
+//     formData = JSON.parse(savedstringFormData);
+//     refs.input.value = formData.email || '';
+//     refs.textarea.value = formData.message || '';
+//   }
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   console.log('Відправляємо форму');
+//   console.log(formData);
+//   evt.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+/**
+ *     Інший спосіб
+ */
+
+// const formEl = document.querySelector('.feedback-form');
+
+// formEl.addEventListener('input', throttle(onFormInput, 500));
+// formEl.addEventListener('submit', onFormSubmit);
+
+// const STORAGE_KEY = 'feedback-form-state';
+// let formData = {};
+
+// populateForm();
+
+// function onFormInput(evt) {
+//   formData.email = formEl.elements.email.value;
+//   formData.message = formEl.elements.message.value;
+
+//   console.log(formData);
+
+//   const stringFormData = JSON.stringify(formData);
+
+//   localStorage.setItem(STORAGE_KEY, stringFormData);
+// }
+
+// function populateForm() {
+//   const savedstringFormData = localStorage.getItem(STORAGE_KEY);
+//   console.log(savedstringFormData);
+
+//   if (savedstringFormData) {
+//     formData = JSON.parse(savedstringFormData || {});
+//     console.log(formData);
+//     formEl.elements.email.value = formData.email;
+//     formEl.elements.message.value = formData.message;
+//   }
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   console.log('Відправляємо форму');
+//   console.log(formData);
+//   evt.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+/**
+ *        Варіант із деструктуризацією та заміною об'єкта
+ */
+
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
 const STORAGE_KEY = 'feedback-form-state';
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form  input'),
-  textarea: document.querySelector('.feedback-form  textarea'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput, 500));
-
 let formData = {};
+const {
+  elements: { email, message },
+} = formEl;
 
 populateForm();
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  console.log('Відправляємо форму');
-
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
 function onFormInput(evt) {
-  //   if (evt.target.value) {
-  //     formData[evt.target.name] = evt.target.value;
-  //   } else {
-  //     formData[evt.target.name] = '';
-  //   }
+  formData.email = email.value;
+  formData.message = message.value;
 
-  formData[evt.target.name] = evt.target.value;
-
-  console.log(formData);
+  //   console.log(formData);
 
   const stringFormData = JSON.stringify(formData);
 
@@ -54,19 +139,23 @@ function onFormInput(evt) {
 }
 
 function populateForm() {
-  //   refs.input.value = '';
-  //   refs.textarea.value = '';
   const savedstringFormData = localStorage.getItem(STORAGE_KEY);
-  const savedFormData = JSON.parse(savedstringFormData);
+  //   console.log(savedstringFormData);
 
-  //   console.log(savedFormData);
-  //   console.log(savedFormData.email);
-  //   console.log(savedFormData.message);
-
-  if (savedFormData) {
-    refs.input.value = savedFormData.email || '';
-    refs.textarea.value = savedFormData.message || '';
-    formData.email = savedFormData.email;
-    formData.message = savedFormData.message;
+  if (savedstringFormData) {
+    formData = JSON.parse(savedstringFormData);
+    console.log(formData);
+    email.value = formData.email;
+    message.value = formData.message;
   }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Відправляємо форму');
+  console.log(formData);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
